451c157fcbb493195d8b81c12646a80214de6b8a
blaiseitt <blazej.poniatowski@gmail.com>
2026-04-29 14:58:17 +0200

    fix for integration test

--- flight-tracker-service/src/main/java/com/flightplatform/tracker/config/MongoConfig.java
+++ flight-tracker-service/src/main/java/com/flightplatform/tracker/config/MongoConfig.java
@@ -18,20 +18,24 @@ public class MongoConfig {
 
     @PostConstruct
     public void createIndexes() {
-        log.info("Creating MongoDB indexes for flights collection...");
-        IndexOperations indexOps = mongoTemplate.indexOps("flights");
+        try {
+            log.info("Creating MongoDB indexes for flights collection...");
+            IndexOperations indexOps = mongoTemplate.indexOps("flights");
 
-        indexOps.ensureIndex(new Index().on("callsign", Sort.Direction.ASC).sparse());
-        indexOps.ensureIndex(new Index().on("lastSeen", Sort.Direction.DESC));
-        indexOps.ensureIndex(new Index().on("originCountry", Sort.Direction.ASC));
-        indexOps.ensureIndex(new Index().on("squawk", Sort.Direction.ASC).sparse());
+            indexOps.ensureIndex(new Index().on("callsign", Sort.Direction.ASC).sparse());
+            indexOps.ensureIndex(new Index().on("lastSeen", Sort.Direction.DESC));
+            indexOps.ensureIndex(new Index().on("originCountry", Sort.Direction.ASC));
+            indexOps.ensureIndex(new Index().on("squawk", Sort.Direction.ASC).sparse());
 
-        indexOps.ensureIndex(
-                new Index()
-                        .on("onGround", Sort.Direction.ASC)
-                        .on("lastSeen", Sort.Direction.DESC)
-        );
+            indexOps.ensureIndex(
+                    new Index()
+                            .on("onGround", Sort.Direction.ASC)
+                            .on("lastSeen", Sort.Direction.DESC)
+            );
 
-        log.info("MongoDB indexes created successfully");
+            log.info("MongoDB indexes created successfully");
+        } catch (Exception e) {
+            log.warn("Could not create MongoDB indexes on startup: {}", e.getMessage());
+        }
     }
 }
