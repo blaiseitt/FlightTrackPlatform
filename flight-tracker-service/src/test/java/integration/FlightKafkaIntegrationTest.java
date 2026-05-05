5e0003cb2e57d09268a0c7148d47e7f0a5799939
blaiseitt <blazej.poniatowski@gmail.com>
2026-05-06 00:14:43 +0200

    fix: FlightKafkaIntegrationTest to work with postgres

--- flight-tracker-service/src/test/java/integration/FlightKafkaIntegrationTest.java
+++ flight-tracker-service/src/test/java/integration/FlightKafkaIntegrationTest.java
@@ -3,15 +3,19 @@ package integration;
 import com.flightplatform.common.event.FlightPositionEvent;
 import com.flightplatform.common.event.KafkaTopics;
 import com.flightplatform.tracker.FlightTrackerServiceApplication;
+import com.flightplatform.tracker.jpa.repo.AirlineRepository;
 import com.flightplatform.tracker.mongo.domain.Flight;
 import com.flightplatform.tracker.mongo.repo.FlightRepository;
+import com.flightplatform.tracker.service.AirlineCache;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.kafka.ConfluentKafkaContainer;
@@ -37,10 +41,28 @@ class FlightKafkaIntegrationTest {
             DockerImageName.parse("mongo:7")
     );
 
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres:16")
+    )
+            .withDatabaseName("flightplatform")
+            .withUsername("flightuser")
+            .withPassword("flightpass");
+
+    @MockBean
+    AirlineRepository airlineRepository;
+
+    @MockBean
+    AirlineCache airlineCache;
+
     @DynamicPropertySource
     static void overrideProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
         registry.add("spring.data.mongodb.uri", mongo::getReplicaSetUrl);
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.flyway.enabled", () -> false);
     }
 
     @Autowired
